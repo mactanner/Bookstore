2599a01369aa7d1785505055b532d3e58ebb6bd9
beeet <beeet@192.168.0.102>
2013-11-09 17:15:46 +0100

    add guava lib add translations add some composites implement locale

---
+++ amasoon/src/main/java/ch/bfh/amasoon/view/UserLocaleBean.java
@@ -0,0 +1,38 @@
+package ch.bfh.amasoon.view;
+
+import com.google.common.base.Objects;
+import java.io.Serializable;
+import java.util.Locale;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author beeet
+ */
+@Named("userLocale")
+@SessionScoped
+public class UserLocaleBean implements Serializable {
+    private static final FacesContext context = FacesContext.getCurrentInstance();
+    private static final ExternalContext extContext = context.getExternalContext();
+    private Locale locale;
+
+    public String getLocale() {
+        return Objects.firstNonNull(locale, fromHttpRequest()).getLanguage();
+    }
+
+    private Locale fromHttpRequest() {
+        return Objects.firstNonNull(extContext.getRequestLocale(), Locale.ENGLISH);
+    }
+
+    public void switchToEnglish() {
+        locale = Locale.ENGLISH;
+    }
+
+    public void switchToGerman() {
+        locale = Locale.GERMAN;
+    }
+
+}
